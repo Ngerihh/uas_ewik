03fe275c508a97d05a740e82018dad43c6ddf908
Ngerihh <Ngerihh@gmail.com>
2023-06-05 14:02:13 +0800

    mengapload file gis

---
+++ data/Pertamina_1.js
@@ -0,0 +1,10 @@
+var json_Pertamina_1 = {
+"type": "FeatureCollection",
+"name": "Pertamina_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama_PRT": "PERTAMINA BARU", "image": "D:\\gis\\image\\pertamina baru.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.480039071139458, -9.461513038901694 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama_PRT": "SPBU NAESLEU", "image": "D:\\gis\\image\\pertamina baru.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.482904980501928, -9.457832707127091 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama_PRT": "PERTAMINA KM4", "image": "D:\\gis\\image\\pertamina baru.jpg" }, "geometry": { "type": "Point", "coordinates": [ 124.481905502018407, -9.471860323599985 ] } }
+]
+}
